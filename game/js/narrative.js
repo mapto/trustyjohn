dafa6715b4d7958c0b2c55c6396bed68df308b99
Martin Ruskov <martin.ruskov@yahoo.com>
2021-11-09 09:42:21 +0100

    Update narrative.js

--- game/js/narrative.js
+++ game/js/narrative.js
@@ -1,6 +1,7 @@
 // TODO will handle content here
 
-// TODO extract and load dynamically
+// TODO extract and load dynamically from a CSV extracted from https://docs.google.com/spreadsheets/d/1N2TX5G59T9z6tlyIEbXTSZCaHk09DXstBK6leT2r4Mk
+// first row in file is headings (dictionary keys), rows that start with # are separators for easier readibility by writers
 events = [{
         // text uses single quotes because it could probably contain more double quotes, so less need to escape chars
         "text": '<p>Once upon a time there was an old king who was so ill that he thought to himself,"I am lying on what must be my death-bed." Then said he, "Tell Faithful John to come to me." Faithful John are you, and are so called, because you have been so loyal to him for your whole life long.</p><p class="instructions">Swipe card left or right to continue...</p>',
