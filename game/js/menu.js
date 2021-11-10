d5887edd01f9d91db854036bc31a60848788a504
Martin Ruskov <martin.ruskov@yahoo.com>
2021-11-10 07:14:25 +0100

    How we could test the narrative

--- game/js/menu.js
+++ game/js/menu.js
@@ -2,6 +2,8 @@
 
 
 function startup() {
+    // TODO allow starting from a different story point for easier development
+    // Can be done by adding a text box in the page. In later versions it will be hidden if not in development mode
     load_card(events['0'])
 }
 
