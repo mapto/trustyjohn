a55e3b8e1d268a54686fa66fd50d397d60400611
Martin Ruskov <martin.ruskov@unimi.it>
2021-11-10 15:42:50 +0100

    Added background music

--- game/js/menu.js
+++ game/js/menu.js
@@ -1,7 +1,33 @@
 // TODO will handle menus and credits here
 
+function switch_sound() {
+    var sound = document.querySelector("audio")
+    var btn = document.querySelector("#sound_switch")
+    if (sound.muted || sound.paused) {
+        sound.muted = false
+        sound.play()
+    } else {
+        sound.muted = true
+        sound.pause()
+    }
+    // Button indicates future state upon click
+    if (sound.muted || sound.paused) {
+        btn.src = "img/btn/sound.png"
+    } else {
+        btn.src = "img/btn/mute.png"
+    }
+}
+
+function init_sound() {
+    var btn = document.querySelector("#sound_switch")
+    btn.onclick = switch_sound
+}
 
 function init_game() {
+    // TODO sound needs to be proposed in menu and enabled if chosen by player
+    init_sound()
+
+    document.querySelector("audio").muted = 
     console.log("Init")
     load_story()
 }
